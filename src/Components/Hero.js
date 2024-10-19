657da7b655d6e4656955b285453c5c6219286bb7
Adhil Muhammed <adhilek46@gmail.com>
2024-10-19 23:47:17 +0530

    commit

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import pic from '../Components/pic.png'
+import pic from '../Components/pic1.png'
 import Cutout from '../Components/Cutout.png'
 
 function Hero() {
   return (
-    <div className='mx-4 pt-20 lg:pt-32 flex flex-col lg:flex-row w-full h-auto lg:h-screen justify-center items-center bg-[#faf9ec]'>
+    <div className='px-6 pt-24 lg:pt-32 flex flex-col lg:flex-row w-full h-auto lg:h-screen justify-center items-center bg-[#faf9ec]'>
       <div className='flex justify-center items-center w-full lg:w-1/2 mb-8 lg:mb-0'>
-        <img src={pic} alt='pic' className='hidden md:object-contain max-w-xs lg:max-w-full' /> 
-        <img src={Cutout} alt='pic' className=' md:hidden object-contain max-w-xs lg:max-w-full' /> 
-        
+        <img src={pic} alt='pic' className='hidden md:block object-contain max-h-[850px] lg:max-w-full' /> 
+        <img src={Cutout} alt='cutout' className='md:hidden object-contain max-w-xs lg:max-w-full' /> 
       </div>
       <div className='flex flex-col w-full lg:w-1/2 justify-center items-start text-center lg:text-left'>
         <h1 className='text-4xl lg:text-6xl mb-4 lg:mb-6'>Welcome,</h1>
         <h2 className='text-3xl lg:text-5xl mb-4 lg:mb-6'>I'm Bhasil Muhammed</h2>
-        <h3 className='text-xl lg:text-3xl px-4 lg:px-0'>
+        <h3 className='text-xl text-start lg:text-3xl'>
           Ex-UPSC aspirant turned entrepreneur, I built Civils360 to empower future civil servants. Passionate about learning and exploration, this site is where those journeys meet!
         </h3>
       </div>
@@ -22,3 +21,8 @@ function Hero() {
 }
 
 export default Hero
+
+
+
+
+
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     <div className='mx-4 text-xl flex lg:w-1/3 font-bold'>
         <h2>Bhasil Muhammed</h2>
     </div>
-    <div className='lg:flex mx-12  lg:w-2/3 justify-center items-center text-xl font-light gap-16'>
+    <div className='hidden md:lg:flex mx-12  lg:w-2/3 justify-center items-center text-xl font-light gap-16'>
         <p>Home</p>
         <p>About</p>
         <p>MileStone</p>
