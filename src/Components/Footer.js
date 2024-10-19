b57f87c113df1a0f7d1e8f05e96cbc07f49a7ab1
Adhil Muhammed <adhilek46@gmail.com>
2024-10-20 01:08:41 +0530

    commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,13 +5,24 @@ import { MdMarkEmailUnread } from "react-icons/md";
 
 function Footer() {
   return (
-    <div className='py-6 w-full flex flex-col items-center justify-center bg-[#e4bfac]  gap-6'>
+    <div className='py-6 w-full flex flex-col items-center justify-center bg-[#c8815d] gap-6'>
       <div className='pt-6 flex flex-row gap-6 text-2xl md:gap-24 md:text-5xl pb-6'>
-        <FaInstagram />
-        <FaFacebook />
-        <MdMarkEmailUnread />
+        
+        <a href="https://www.instagram.com/tryst_with_bhasi/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+       
+        <a href="https://www.facebook.com/basil.muhammed.9461" target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
+       
+        <a href="mailto:bhasilek@gmail.com" target="_blank" rel="noopener noreferrer">
+          <MdMarkEmailUnread />
+        </a>
       </div>
+
       <p className='text-sm'>Website By Crown</p>
+      
     </div>
   )
 }
