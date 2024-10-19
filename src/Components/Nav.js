ca5d42b2fe7cce4aa5fd22641bcbd449b7c8d40d
Adhil Muhammed <adhilek46@gmail.com>
2024-10-20 01:32:11 +0530

    commit

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Nav() {
-  // Function to handle scroll to the corresponding section
+  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+
+  
   const handleScrollTo = (id) => {
     const section = document.getElementById(id);
     if (section) {
-      section.scrollIntoView({ behavior: 'smooth' }); // Smooth scrolling effect
+      section.scrollIntoView({ behavior: 'smooth' }); 
     }
+  };
+
+  
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -19,6 +26,29 @@ function Nav() {
         <p onClick={() => handleScrollTo('about')} className="cursor-pointer">About</p>
         <p onClick={() => handleScrollTo('roadmap')} className="cursor-pointer">MileStone</p>
         <p onClick={() => handleScrollTo('contact')} className="cursor-pointer">Contact</p>
+      </div>
+      
+      <div className='md:hidden mx-4'>
+        <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
+          {isMenuOpen ? '✖️' : '☰'} 
+        </button>
+      </div>
+      
+      <div 
+        className={`fixed top-0 right-0 h-full w-64 bg-[#a45933] shadow-lg transform transition-transform duration-300 ease-in-out ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}
+      >
+        
+        <div className='flex justify-end p-4'>
+          <button onClick={toggleMenu} className='text-2xl focus:outline-none'>
+            ✖️ 
+          </button>
+        </div>
+        <div className='flex flex-col px-6 items-center justify-start h-full text-xl font-light'>
+          <p onClick={() => { handleScrollTo('hero'); setIsMenuOpen(false); }} className="cursor-pointer py-4">Home</p>
+          <p onClick={() => { handleScrollTo('about'); setIsMenuOpen(false); }} className="cursor-pointer py-4">About</p>
+          <p onClick={() => { handleScrollTo('roadmap'); setIsMenuOpen(false); }} className="cursor-pointer py-4">MileStone</p>
+          <p onClick={() => { handleScrollTo('contact'); setIsMenuOpen(false); }} className="cursor-pointer py-4">Contact</p>
+        </div>
       </div>
     </div>
   );
